162ccb90bf045552ef3fd76e1e3bc5c37ec0d87d
Daniyar Sabirov <sabirov.astana@gmail.com>
2025-12-07 20:44:32 +0100

    recet counter

---
+++ src/queue/recentCounter.ts
@@ -0,0 +1,15 @@
+class RecentCounter {
+  private queue: number[];
+
+  constructor() {
+    this.queue = [];
+  }
+
+  ping(t: number): number {
+    this.queue.push(t);
+    while (this.queue[0] < t - 3000) {
+      this.queue.shift();
+    }
+    return this.queue.length;
+  }
+}
